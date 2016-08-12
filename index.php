c32e5153cedf6949e2841268caf5610327aab502
kamelot2012 <kamelot2012@mail.ru>
2016-08-12 18:56:22 +0300

    создание

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 12.08.2016
+ * Time: 14:18
+ */
+require_once('db_config');
--- www/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Admin
- * Date: 11.08.2016
- * Time: 21:42
- */
-phpinfo();
